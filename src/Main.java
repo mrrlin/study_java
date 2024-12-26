86aaa3db924fd784808aa9a1d124f696b8693a0f
Angelina <angelina.kulyaeva@yandex.ru>
2024-12-26 23:14:22 +0300

    easy peasy lemon squeezy

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Система рассчета стоимости топлива");
+
+        int fuelType = 92;
+        int amount = 50;
+
+        double fuel92Price = 60.2;
+        double fuel95Price = 67.33;
+
+        double fuelPrice = 0;
+
+        if (fuelType == 92) {
+            fuelPrice = fuel92Price;
+        } else if (fuelType == 95) {
+            fuelPrice = fuel95Price;
+        } else {
+            System.out.println("Указан неверный тип топлива.");
+        }
+
+        if (amount < 1) {
+            System.out.println("Указано слишком малое количество топлива");
+            amount = 0;
+        }
+
+        System.out.println("Цена выбранного топлива: " + fuelPrice + " руб.");
+
+        double totalPrice = fuelPrice * amount;
+
+        System.out.println("Общая стоимость заправки: " + totalPrice + " руб.");
+
+        System.out.println(fuelType);
+    }
+}
